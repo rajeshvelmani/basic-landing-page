18ea4c44a8fd69fbd3d68e403b695d63efa28bb6
rajeshvelmani <rajeshvelmani@gmail.com>
2020-04-05 15:35:38 +0530

    splash screen added

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -7,3 +7,14 @@ function toggleFunction() {
     navmenu.classList.toggle("active");
     toggleBtn.classList.toggle("active");
 }
+
+var body = document.querySelector("body");
+var container = document.querySelector(".container");
+
+//body.style.overflow = "hidden";
+container.style.display = "none";
+window.addEventListener("load", function () {
+    container.style.display = "block";
+    // body.style.overflow = "auto";
+    body.classList.add("page-loads");
+});
